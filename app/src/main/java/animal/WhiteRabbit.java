ae49bdb9f7244e1d7e92114eccf0b8169ef9e37d
Lei Chen <mashirocl@gmail.com>
2023-01-23 13:06:46 +0900

    ref: Rename class Rabbit to WhiteRabbit

--- app/src/main/java/animal/WhiteRabbit.java
+++ app/src/main/java/animal/WhiteRabbit.java
@@ -4,7 +4,7 @@ package animal;
  * @author mashirocl@gmail.com
  * @since 2023/01/23 12:55
  */
-public class Rabbit extends Animal{
+public class WhiteRabbit extends Animal{
     private String name = "Rabbit";
     private String sound = "RabiRabi";
 
