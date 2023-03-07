79e9a0104ea9879229bacb4f9571de6e1951eaea
Lei Chen <mashirocl@gmail.com>
2023-03-07 14:18:27 +0900

    update: add javadoc for Ramen

--- app/src/main/java/food/Ramen.java
+++ app/src/main/java/food/Ramen.java
@@ -6,6 +6,12 @@ import java.util.List;
  * @author mashirocl@gmail.com
  * @since 2023/03/07 14:07
  */
+
+/**
+ * Ramen
+ * Delicious, expensive
+ * Bring happiness when feeling hungry
+ */
 public class Ramen {
     public String name = "Ramen";
 
