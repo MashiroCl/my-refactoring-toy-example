52e398e8c29664f5c9851b2856e8b566052faab3
Lei Chen <mashirocl@gmail.com>
2024-01-09 13:25:21 +0100

    update: revsersed the if condition

--- app/src/main/java/code/Condition.java
+++ app/src/main/java/code/Condition.java
@@ -25,15 +25,15 @@ public class Condition {
         boolean flag = true;
         int a=3, b=2;
         int res = 0;
-        if(flag){
-            res = a+b;
-            res = res*a;
-            res = res*b;
-        }
-        else{
+        if(!flag){
             res = a-b;
             res = res/a;
             res = res/b;
+        }
+        else{
+            res = a+b;
+            res = res*a;
+            res = res*b;
         }
         System.out.println(res);
     }
