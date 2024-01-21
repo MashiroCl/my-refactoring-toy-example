273f1fa52d3bcc4d464bd0b30fb863f812f25852
Lei Chen <mashirocl@gmail.com>
2024-01-21 19:01:56 +0100

    update: convert conditional to switch

--- app/src/main/java/code/Condition.java
+++ app/src/main/java/code/Condition.java
@@ -148,16 +148,18 @@ public class Condition {
 
     public void conditionalToSwitch(){
         int a = 1;
-        if(a==1){
-            System.out.println("1");
-        }
-        else if(a==2){
-            System.out.println("2");
-        }
-        else if(a==3){
-            System.out.println("3");
-        }else{
-            System.out.println("is not 1 or 2 or 3");
+        switch (a){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            default:
+                System.out.println("is not 1 or 2 or 3");
         }
     }
 
