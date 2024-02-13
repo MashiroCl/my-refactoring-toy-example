1b4c3a838c2e8fe1e72b42f0be4cdb0d12f96cc0
Lei Chen <mashirocl@gmail.com>
2024-02-13 12:11:59 +0100

    feat: assertTrue to assertEqual pattern

---
+++ app/src/main/java/code/Pattern.java
@@ -0,0 +1,15 @@
+package code;
+
+/**
+ * @author mashirocl@gmail.com
+ * @since 2024/02/13 12:01
+ */
+public class Pattern {
+    public void change(){
+        int a = 1;
+        int b = 2;
+        int c = a+b;
+        assertTrue(a==1);
+        System.out.println(c);
+    }
+}
