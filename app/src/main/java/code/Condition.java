0c2167cd704f90d0bde8ad474c157d558b13fa74
Lei Chen <mashirocl@gmail.com>
2024-03-25 16:27:31 +0100

    update: changeLogicOperator

--- app/src/main/java/code/Condition.java
+++ app/src/main/java/code/Condition.java
@@ -393,7 +393,7 @@ public class Condition {
     public void changeLogicOperator(){
         int a = 1;
         int b = 2;
-        if(a==1 || b==2){
+        if(a==1 && b==2){
             System.out.println("this is great");
         }
     }
