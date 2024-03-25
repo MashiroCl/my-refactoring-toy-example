e3d856e1ba4c3b85e8bce83c84f1396a3d8f31b4
Lei Chen <mashirocl@gmail.com>
2024-03-25 15:39:25 +0100

    feat: changeLogicOperator

--- app/src/main/java/code/Condition.java
+++ app/src/main/java/code/Condition.java
@@ -389,4 +389,16 @@ public class Condition {
         }
 
     }
+
+    public void changeLogicOperator(){
+        int a = 1;
+        int b = 2;
+        if(a==1 && b==2){
+            System.out.println("this is great");
+        }
+
+        if(a==1 || b==2){
+            System.out.println("this is also great");
+        }
+    }
 }
