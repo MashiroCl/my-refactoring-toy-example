8d125b795cd6c352762e617916ee4ccef04f4bdb
Lei Chen <mashirocl@gmail.com>
2025-04-16 22:33:04 +0900

    file for loop

---
+++ app/src/main/java/code/LoopChange.java
@@ -0,0 +1,13 @@
+package code;
+
+public class LoopChange {
+
+    public void LoopRelatedChagneDisplay(){
+        int n = 5;
+
+        for(int i=0;i<n;i++){
+            System.out.println("Hello");
+        }
+
+    }
+}
