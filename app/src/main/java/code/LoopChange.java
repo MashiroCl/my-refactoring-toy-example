9bb4eedf2f8afc654212388ac897c01d3bcaa361
Lei Chen <mashirocl@gmail.com>
2025-04-19 21:44:05 +0900

    update: change loop initializer type

--- app/src/main/java/code/LoopChange.java
+++ app/src/main/java/code/LoopChange.java
@@ -25,7 +25,7 @@ public class LoopChange {
 
     public void modifyLoopInitializer() {
         List<String> list = new LinkedList<>();
-        for (Integer i = 0; i < list.size(); i++) {
+        for (var i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
 
