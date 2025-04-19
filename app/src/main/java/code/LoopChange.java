4f0b48d7762ea952b449086e32aca21d80c6f7a6
Lei Chen <mashirocl@gmail.com>
2025-04-19 21:21:55 +0900

    feat: add modifyLoopInitializer

--- app/src/main/java/code/LoopChange.java
+++ app/src/main/java/code/LoopChange.java
@@ -1,12 +1,15 @@
 package code;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class LoopChange {
 
     public void LoopRelatedChagneDisplay() {
         int n = 5;
         int i = 0;
 
-        while (i<n) {
+        while (i < n) {
             System.out.println("Hello");
             i++;
         }
@@ -18,5 +21,13 @@ public class LoopChange {
         for (int i = 0; i < l.length; i++) {
             System.out.println(l[i]);
         }
+    }
+
+    public void modifyLoopInitializer() {
+        List<String> list = new LinkedList<>();
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
     }
 }
